28e4876af8fd681ff870fdeb8fac9852c74127c6
donice <doniceubaru1@gmail.com>
2025-06-07 09:58:29 +0100

    FIxed CTA links

--- app/dashboard/index.tsx
+++ app/dashboard/index.tsx
@@ -1,57 +1,71 @@
-"use client"
-import {
-  Card,
-  CardContent,
-  CardHeader,
-} from '@/components/ui/card';
-import { useSession } from 'next-auth/react';
+"use client";
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
 import { RiFundsLine } from "react-icons/ri";
 import { PiUserSwitch } from "react-icons/pi";
 import { TbChartCandle, TbShoppingBag } from "react-icons/tb";
+import { useRouter } from "next/navigation";
 
 export default function CTA() {
+  const router = useRouter();
 
   return (
     <div className="flex flex-col gap-4">
       <h1 className="text-2xl font-bold">Trade Dashboard</h1>
 
       <div className="grid gap-4 grid-cols-2 md:grid-cols-4 lg:grid-cols-4">
-        <Card className='text-white bg-red-900 border-2 border-red-500'>
+        <Card
+          onClick={() => router.push("/dashboard/deposit")}
+          className="text-white bg-red-900 border-2 border-red-500"
+        >
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <p className="text-sm font-medium">Fund Account</p>
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold"><RiFundsLine className='text-3xl' /></div>
+            <div className="text-2xl font-bold">
+              <RiFundsLine className="text-3xl" />
+            </div>
           </CardContent>
         </Card>
-        <Card className='text-white bg-blue-900 border-2 border-blue-500'>
+        <Card
+          onClick={() => router.push("/dashboard/traders")}
+          className="text-white bg-blue-900 border-2 border-blue-500"
+        >
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <p className="text-sm font-medium">Copy Traders</p>
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold"><PiUserSwitch className='text-3xl' /></div>
+            <div className="text-2xl font-bold">
+              <PiUserSwitch className="text-3xl" />
+            </div>
           </CardContent>
         </Card>
-        <Card className='text-white bg-green-900 border-2 border-green-500'>
+        <Card
+          onClick={() => router.push("/dashboard/assets")}
+          className="text-white bg-green-900 border-2 border-green-500"
+        >
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <p className="text-sm font-medium">Asset Markets</p>
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold"><TbShoppingBag className='text-3xl' /></div>
-
+            <div className="text-2xl font-bold">
+              <TbShoppingBag className="text-3xl" />
+            </div>
           </CardContent>
         </Card>
-        <Card className='text-white bg-purple-900 border-2 border-purple-500'>
+        <Card
+          onClick={() => router.push("/dashboard/trade")}
+          className="text-white bg-purple-900 border-2 border-purple-500"
+        >
           <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
             <p className="text-sm font-medium">Trading room</p>
           </CardHeader>
           <CardContent>
-            <div className="text-2xl font-bold"><TbChartCandle className='text-3xl' /></div>
+            <div className="text-2xl font-bold">
+              <TbChartCandle className="text-3xl" />
+            </div>
           </CardContent>
         </Card>
       </div>
-
-
     </div>
   );
 }
